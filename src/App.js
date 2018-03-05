febf0d1c948a505ccf33c235da8781c788b5a9b7
Joao Moreira <jfmoreir@thoughtworks.com>
2018-03-05 09:59:07 -0300

    Creating repository

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Book from './components/Book';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      author: '',
+      books: []
+    };
+    this.saveBook = this.saveBook.bind(this);
+  }
+
+  saveBook(event) {
+    event.preventDefault();
+    this.setState(
+      {
+        books: this.state.books.concat(
+          {
+            title: this.state.title,
+            author: this.state.author
+          }
+        )
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.saveBook}>
+          <h2>Add Books</h2>
+          <input type="input" onChange={(event) => this.setState({title: event.target.value})}/>
+
+          <input type="input" onChange={(event) => this.setState({author: event.target.value})}/>
+
+          <button>Create Book</button>
+        </form>
+        {this.state.books.map(book => <Book key={book.title} title={book.title} author={book.author} />)}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Book = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <h2>{props.author}</h2>
+    </div>
+  );
+}
+
+export default Book;
