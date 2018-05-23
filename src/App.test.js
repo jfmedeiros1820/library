b3242987097f74787b314e435592c1f46100f9ad
Joao Moreira <jfmoreir@thoughtworks.com>
2018-05-23 13:48:57 -0300

    [Joao/Fernanda] Including enzyme and unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Library', () => {
+
+  it('renders the title', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h2').text()).toEqual('Add Books');
+  });
 });
+
